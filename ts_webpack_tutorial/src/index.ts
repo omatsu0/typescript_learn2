f6cb5ca191db3374ce51cb7a6cd29534fca3e877
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2025-12-16 15:20:50 +0900

    Display Welcome on the screen

---
+++ ts_webpack_tutorial/src/index.ts
@@ -0,0 +1,4 @@
+const message: string = "Hello TypeScript + Webpack!";
+console.log(message);
+
+document.body.innerHTML += `<p>${message}</p>`;
---
+++ ts_webpack_tutorial/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+    hot: true
+  },
+};
